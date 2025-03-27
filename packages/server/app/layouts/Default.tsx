46f91c123e946533f5b6a5a1cd5919dd3cf3dee3
Styles <kartik100100@gmail.com>
2025-03-27 14:49:51 +0530

    init ui

--- packages/server/app/layouts/Default.tsx
+++ packages/server/app/layouts/Default.tsx
@@ -7,30 +7,9 @@ import { setAuthToken } from "../shared/utils/apiClient";
 import { Toaster } from "sonner";
 
 export default function () {
-  // useEffect(() => {
-  //   if (ready && authenticated) {
-  //     getAccessToken().then((token) => {
-  //       token && setAuthToken(token);
-  //     });
-  //   }
-  // }, [ready, authenticated]);
-
   return (
     <main className="h-screen flex flex-col relative">
       <Header />
-
-      <div
-        className={cn(
-          "flex-1 overflow-y-scroll pb-[10vh] duration-300",
-          !true && "opacity-0 scale-50 saturate-0"
-        )}
-      >
-        <Outlet />
-      </div>
-
-      <Tabs />
-
-      <Toaster richColors mobileOffset={{ bottom: 64 }} />
     </main>
   );
 }
