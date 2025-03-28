4bc7569b6449a077c2e8fb96f6a61444043b6dd8
jriyyya <riya1jain567@gmail.com>
2025-03-28 17:17:28 +0530

    Add database schema

---
+++ packages/server/api/lib/db/schema/reply.ts
@@ -0,0 +1,14 @@
+import { sqliteTable as table } from "drizzle-orm/sqlite-core";
+import * as t from "drizzle-orm/sqlite-core";
+import { users } from "./user";
+import { posts } from "./post";
+import { timestamps } from "./helpers";
+
+export const replies = table("replies", {
+    id: t.int().primaryKey({ autoIncrement: true }),
+    userId: t.int().references(() => users.id).notNull(),
+    postId: t.int().references(() => posts.id),
+    parentReplyId: t.int().references(() => posts.id),
+    content: t.text().notNull(),
+    ...timestamps
+});
--- packages/server/api/lib/db/schema/user.ts
+++ packages/server/api/lib/db/schema/user.ts
@@ -1,12 +1,23 @@
 import { sqliteTable as table } from "drizzle-orm/sqlite-core";
 import * as t from "drizzle-orm/sqlite-core";
 import { suiAddressType, timestamps } from "./helpers";
+import { relations } from "drizzle-orm";
+import { posts } from "./post";
 
 export const users = table("users", {
     id: t.int().notNull().primaryKey({ autoIncrement: true }),
-    identifier: suiAddressType().notNull(),
+    identity: suiAddressType().notNull(),
+    username: t.text().notNull(),
+    fullName: t.text().notNull(),
+    wallet: suiAddressType().notNull(),
+    suins_domain_name: t.text(),
+    image_url: t.text(),
+    banner_url: t.text(),
+    about: t.text(),
     imageUrl: t.text().notNull().default(""),
+    timezone: t.int(),
+    pinned: t.int("pinned_post").references((): t.AnySQLiteColumn => posts.id),
     ...timestamps,
 }, (table) => [
-    t.uniqueIndex("identifier_idx").on(table.identifier),
+    t.uniqueIndex("identity_idx").on(table.identity),
 ]);
