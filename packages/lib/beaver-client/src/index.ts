9a83134a29d26e3000d1c3f01b5ee6fc610fcb8b
Riya Jain <riya1jain567@gmail.com>
2025-04-03 20:33:07 +0530

    Add interaction for server and sui in sdk

--- packages/lib/beaver-client/src/index.ts
+++ packages/lib/beaver-client/src/index.ts
@@ -4,15 +4,20 @@ import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
 import { BeaverClientConfig } from "./types";
 import type { API } from "server"
 import { hc } from "hono/client"
-import { DefaultC } from "./default";
+import { Defaults } from "./default";
 import { S3Client } from "@aws-sdk/client-s3";
+import { tryCatch } from "./utils/tryCatch";
+import { User } from "./user";
+
 // import { User } from "./user";
 
 /**
  * Main client for interacting with the Beaver Social Layer.
 */
-export class BeaverClient extends DefaultC {
+export class BeaverClient {
     config: BeaverClientConfig;
+    defaults: Defaults;
+    logger: Logger;
 
     constructor(config: BeaverClientConfig) {
         const logger = new Logger("Beaver Social SDK", Boolean(config.debug));
@@ -21,13 +26,34 @@ export class BeaverClient extends DefaultC {
         const apiClient = hc<typeof API>(config.apiBaseUrl || "https://api.beaversocial.com/api");
         const s3Client = new S3Client();
 
-        super(apiClient, suiClient, s3Client, logger);
+        this.defaults = {
+            apiClient,
+            suiClient,
+            s3Client,
+            contracts: null,
+        }
+
         this.config = config;
+        this.logger = logger;
 
         this.logger.info("Client Initialising", config);
     }
 
-    // get user() {
-    //     return new User(this.apiClient, this.suiClient, this.logger);
-    // }
+    public async initialize() {
+        const contractsResponse = await tryCatch(this.defaults.apiClient.contracts.$get())
+        if (contractsResponse.error)
+            return this.logger.error("Unable to fetch contract details from server");
+
+        const contracts = await tryCatch(contractsResponse.data.json())
+        if (contracts.error)
+            return this.logger.error("Invalid contracts retreived from server")
+
+        this.defaults.contracts = contracts.data;
+    }
+
+    get user() {
+        return new User(this.defaults, this.logger);
+    }
 }
+
+
