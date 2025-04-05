e8b39a0e9fdb16dacc0cb22784243b1d7ef8d4f0
Spandan Barve <spandan567@gmail.com>
2025-04-05 17:00:57 +0530

    someone fix tsconfig error

--- packages/lib/beaver-client/src/identity/index.ts
+++ packages/lib/beaver-client/src/identity/index.ts
@@ -1,51 +1,37 @@
-import { Transaction } from "@mysten/sui/transactions"
+import { Transaction } from "@mysten/sui/transactions";
 import { Defaults } from "../types";
 import Logger from "../logger";
-import { bcs } from '@mysten/bcs';
+import { bcs } from "@mysten/bcs";
 
 export class Identity {
-    defaults: Defaults
-    logger: Logger
+  defaults: Defaults;
+  logger: Logger;
 
-    constructor(defaults: Defaults, logger: Logger) {
-        this.defaults = defaults;
-        this.logger = logger;
+  constructor(defaults: Defaults, logger: Logger) {
+    this.defaults = defaults;
+    this.logger = logger;
 
-        this.logger.info("User interface instantiated");
-    }
+    this.logger.info("User interface instantiated");
+  }
 
-    public async newIdentity(options: { username: string, about: string }) {
-        const { contracts, surface, suiClient } = this.defaults;
-        const { username, about } = options;
+  public async mint(options: { username: string; about: string }) {
+    const { contracts, surface, suiClient } = this.defaults;
+    const { username, about } = options;
 
-        if (!contracts) {
-            return this.logger.error("Contracts not initialized. Please call BeaverClient.initialize() first.");
-        }
+    const tx = new Transaction();
 
-        const tx = new Transaction()
+    contracts.registry.mint(tx, {
+      username,
+      about,
+    });
 
-        const registry = tx.object("0x");
-        const clock = tx.object("0x6")
+    const transactionBytes = await tx.build();
 
-        tx.moveCall(
-            {
-                package: contracts.testnet.beaverSocial.package,
-                module: "registry",
-                function: "mint",
-                arguments: [
-                    registry,
-                    bcs.string().serialize(username),
-                    bcs.string().serialize(about),
-                    clock,
-                ]
-            }
-        )
+    const signature = await surface.signTransaction(transactionBytes);
 
-        const transactionBytes = await tx.build();
-
-        const signature = await surface.signTransaction(transactionBytes);
-
-        suiClient.executeTransactionBlock({ transactionBlock: transactionBytes, signature: signature.signature })
-    }
-
+    return suiClient.executeTransactionBlock({
+      transactionBlock: transactionBytes,
+      signature: signature.signature,
+    });
+  }
 }
--- packages/lib/beaver-client/src/index.ts
+++ packages/lib/beaver-client/src/index.ts
@@ -1,60 +1,71 @@
-
 import Logger from "./logger";
-import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 import { BeaverClientConfig, Surface } from "./types";
-import type { API } from "server"
-import { hc } from "hono/client"
+import type { API } from "server";
+import { hc } from "hono/client";
 import { Defaults } from "./types";
 import { S3Client } from "@aws-sdk/client-s3";
 import { tryCatch } from "./utils/tryCatch";
 import { Identity } from "./identity";
+import { Contracts } from "contracts";
 
 // import { User } from "./user";
 
 /**
  * Main client for interacting with the Beaver Social Layer.
-*/
+ */
 export class BeaverClient {
-    config: BeaverClientConfig;
-    defaults: Defaults;
-    logger: Logger;
+  config: BeaverClientConfig;
+  defaults: Defaults;
+  ready: boolean = false;
+  logger: Logger;
 
-    constructor(surface: Surface, config: BeaverClientConfig) {
-        const logger = new Logger("Beaver Social SDK", Boolean(config.debug));
-        const rpcUrl = getFullnodeUrl(config.network || 'mainnet');
-        const suiClient = new SuiClient({ url: rpcUrl });
-        const apiClient = hc<typeof API>(config.apiBaseUrl || "https://api.beaversocial.com/api");
-        const s3Client = new S3Client();
+  constructor(surface: Surface, config: BeaverClientConfig) {
+    const logger = new Logger("Beaver Social SDK", Boolean(config.debug));
+    const rpcUrl = getFullnodeUrl(config.network || "mainnet");
+    const suiClient = new SuiClient({ url: rpcUrl });
+    const apiClient = hc<typeof API>(
+      config.apiBaseUrl || "https://api.beaversocial.com/api"
+    );
+    const s3Client = new S3Client();
 
-        this.defaults = {
-            apiClient,
-            suiClient,
-            s3Client,
-            surface,
-            contracts: null,
-        }
+    this.defaults = {
+      apiClient,
+      suiClient,
+      s3Client,
+      surface,
+      contracts: null,
+    };
 
-        this.config = config;
-        this.logger = logger;
+    this.config = config;
+    this.logger = logger;
+  }
 
-        this.logger.info("Client Initialising", config);
-    }
+  public async initialize() {
+    const contractsResponse = await tryCatch(
+      this.defaults.apiClient.contracts.$get()
+    );
+    if (contractsResponse.error)
+      return this.logger.error("Unable to fetch contract details from server");
 
-    public async initialize() {
-        const contractsResponse = await tryCatch(this.defaults.apiClient.contracts.$get())
-        if (contractsResponse.error)
-            return this.logger.error("Unable to fetch contract details from server");
+    const contracts = await tryCatch(contractsResponse.data.json());
+    if (contracts.error)
+      return this.logger.error("Invalid contracts retreived from server");
 
-        const contracts = await tryCatch(contractsResponse.data.json())
-        if (contracts.error)
-            return this.logger.error("Invalid contracts retreived from server")
+    this.defaults.contracts = new Contracts({
+      packageId: contracts.data.testnet.packages.beaverSocial.id,
+      objects: {
+        adminsRecord: { id: contracts.data.testnet.objects.adminsRecord },
+        clock: { id: contracts.data.testnet.objects.clock },
+        registry: { id: contracts.data.testnet.objects.registry },
+      },
+    });
 
-        this.defaults.contracts = contracts.data;
-    }
+    this.ready = true;
+    this.logger.info("Client Initialised", this.defaults.contracts);
+  }
 
-    get user() {
-        return new Identity(this.defaults, this.logger);
-    }
+  get identity() {
+    return new Identity(this.defaults, this.logger);
+  }
 }
-
-
