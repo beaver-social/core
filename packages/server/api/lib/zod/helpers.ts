331cb0f6e0618cc8255feb0f083e52b91edc7581
Styles <kartik100100@gmail.com>
2025-05-04 20:32:50 +0530

    fixes

--- packages/server/api/lib/zod/helpers.ts
+++ packages/server/api/lib/zod/helpers.ts
@@ -54,15 +54,6 @@ export const zMedia = z.object({
   altText: z.string().optional(),
 });
 
-export const zSwipeMedia = z.object({
-  buffer: z.instanceof(Buffer),
-  thumbnailUrl: z.string().optional(),
-  duration: z.number().optional(),
-  width: z.number().optional(),
-  height: z.number().optional(),
-  altText: z.string().optional(),
-});
-
 export const zJwtPayload = () =>
   z.object({
     app: z.number(),
