b341ff2c20a0e61a1fbac99f76e969a8125e246d
Spandan Barve <spandan567@gmail.com>
2025-05-05 16:41:15 +0530

    Changes se to achha hi hai

--- packages/lib/beaver-client/src/bindings/User.ts
+++ packages/lib/beaver-client/src/bindings/User.ts
@@ -17,7 +17,7 @@ export default class User {
   async register(
     options: Pick<
       ApiParams<Api["users"]["$post"]>["json"],
-      "username" | "fullName" | "about" | "imageUrl" | "bannerUrl"
+      "username" | "fullName" | "about" | "image" | "banner"
     >
   ) {
     const { features, address } = this.defaults.store;
